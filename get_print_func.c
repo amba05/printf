74b42894b020f1a8e8c0c3e453a76355d23feabf
amba05 <emma.amba@gmail.com>
2022-04-17 16:25:47 +0000

    ds

--- get_print_func.c
+++ get_print_func.c
@@ -7,8 +7,7 @@
  */
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 {
-	print_t pr[] =
-       {
+	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str},
 		{"i", print_int}, {"d", print_int},
 		{"b", print_bnr}, {"u", print_unt},
